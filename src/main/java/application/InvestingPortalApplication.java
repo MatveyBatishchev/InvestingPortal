c7dfa36d36cbfe7d8486f66e776a76e55d8492d5
Matvey Batishchev <batishevmatvey@mail.ru>
2023-06-06 14:42:06 +0300

    Initial commit

---
+++ src/main/java/application/InvestingPortalApplication.java
@@ -0,0 +1,13 @@
+package application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestingPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestingPortalApplication.class, args);
+	}
+
+}
